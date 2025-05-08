d3fa6b4795ab73ff4c0b2762e25dde3241b786bc
Pascal Grimm <pascal.grimm@kaufland.de>
2025-05-08 13:36:13 +0200

    added cors to server.js

--- services/flight-service/server.js
+++ services/flight-service/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { connectDB, config } from './db.js';
 import flightRoutes from './flights.js';
+import cors from 'cors';
 
 const app = express();
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(cors());
 
 // Connect to the database
 connectDB();
--- services/hotel-service/server.js
+++ services/hotel-service/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import { connectDB, config } from './db.js';
 import hotelRoutes from './hotels.js';
+import cors from 'cors';
+
 
 const app = express();
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(cors());
 
 // Connect to the database
 connectDB();
