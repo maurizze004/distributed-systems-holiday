cc879a37346f0137eb863057017e2b591a7711ce
PasGrimm14 <pas.grimm.23@heilbronn.dhbw.de>
2025-05-27 22:44:04 +0200

    fix frontend that link to admin only shows if role admin is assigned

--- user-frontend/index.js
+++ user-frontend/index.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
   const username = localStorage.getItem('username');
   const userSpan = document.getElementById('current-user');
+  const role = localStorage.getItem('role');
+  const adminLink = document.getElementById('admin-link');
   if (username) {
     userSpan.textContent = username;
   } else {
     userSpan.textContent = 'Gast';
+  }
+  if (role === 'admin' && adminLink) {
+    adminLink.style.display = 'inline';
+  } else if (adminLink) {
+    adminLink.style.display = 'none';
   }
 });
 
